79b5de1f083ac814d4ac3412f9dc69c121fdf0bf
Lucas Maieski <lucmaieski@gmail.com>
2021-10-19 17:49:31 -0300

    Feito requisito 1

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        conteúdo do Login
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        conteúdo do NotFound
+      </div>
+    );
+  }
+}
+
+export default NotFound;
