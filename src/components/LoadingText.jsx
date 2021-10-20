da1a4d75dabde88695cd4e6dfebcc79fcd3bbb39
Lucas Maieski <lucmaieski@gmail.com>
2021-10-20 15:46:39 -0300

    Feito requsisito 2

---
+++ src/components/LoadingText.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class LoadingText extends React.Component {
+  render() {
+    return (
+      <p>Carregando...</p>
+    );
+  }
+}
+
+export default LoadingText;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,61 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+import { createUser } from '../services/userAPI';
+import LoadingText from '../components/LoadingText';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      buttonState: true,
+      userName: '',
+      loadingText: false,
+      accept: false,
+    };
+    this.saveName = this.saveName.bind(this);
+  }
+
+  enableButton = ({ target }) => {
+    const minInputChar = 3;
+    return (
+      target.value.length >= minInputChar
+        ? this.setState({ buttonState: false },
+          () => this.setState({ userName: target.value }))
+        : this.setState({ buttonState: true })
+    );
+  }
+
+  async saveName() {
+    const { userName } = this.state;
+    this.setState({ loadingText: true });
+    await createUser({ name: userName });
+    this.setState({ loadingText: false, accept: true });
+  }
+
   render() {
+    const { buttonState, loadingText, accept } = this.state;
+    if (loadingText) return <LoadingText />;
+    if (accept) return <Redirect to="/search" />;
     return (
       <div data-testid="page-login">
-        conteúdo do Login
+        <form action="signUp">
+          <input
+            onChange={ this.enableButton }
+            type="text"
+            name="User-Name"
+            id="login-name-input"
+            data-testid="login-name-input"
+          />
+          <button
+            onClick={ this.saveName }
+            disabled={ buttonState }
+            type="button"
+            data-testid="login-submit-button"
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
