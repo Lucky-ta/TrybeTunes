26689f3a0756fb715a152a52200a61448a5bfe49
Lucas Maieski <lucmaieski@gmail.com>
2021-10-20 17:31:12 -0300

    Feito requisito 5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,11 +32,11 @@ class Header extends React.Component {
       return (
         <header data-testid="header-component">
           <h3 data-testid="header-user-name">{ name }</h3>
-          <Link data-testid="link-to-search" to="/search">Search</Link>
+          <Link data-testid="link-to-search" to="/search">Pesquisa</Link>
           <br />
-          <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">Favoritos</Link>
           <br />
-          <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+          <Link data-testid="link-to-profile" to="/profile">Perfil</Link>
         </header>
       );
     }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,40 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      buttonState: true,
+    };
+  }
+
+  enableButton = ({ target }) => {
+    const minCharacters = 2;
+    return target.value.length >= minCharacters
+      ? this.setState({ buttonState: false })
+      : this.setState({ buttonState: true });
+  };
+
   render() {
+    const { buttonState } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        conteúdo do Search
+        <form action="search-musics">
+          <input
+            onChange={ this.enableButton }
+            data-testid="search-artist-input"
+            type="text"
+          />
+          <button
+            disabled={ buttonState }
+            data-testid="search-artist-button"
+            type="button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
